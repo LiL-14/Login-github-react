3b77fa5ddf7aa6f0ebea002788dc5eff1502e68d
Lucas Ballonje <lucas.ballonje@ativo.com>
2022-01-17 16:40:25 -0300

    commit projeto completo

---
+++ src/controllers/ProfilleUserController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { ProfileUserService } from "../services/ProfileUserService";
+
+
+
+
+class ProfileUSerController {
+    async handle(request: Request, response: Response){
+        const { user_id } = request;
+        
+        const service = new ProfileUserService();
+
+        const result = await service.execute(user_id);
+
+        return response.json(result);
+    }
+
+}
+
+
+export { ProfileUSerController }
---
+++ src/routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { Get3LastMessagesController } from "./controllers/GetLast3MessagesController";
+import { ProfileUSerController } from "./controllers/ProfilleUserController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+
+const router = Router();
+
+router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post("/messages", ensureAuthenticated , new CreateMessageController().handle);
+
+router.get("/messages/last3", new Get3LastMessagesController().handle);
+
+router.get("/profile", ensureAuthenticated,new ProfileUSerController().handle);
+
+
+export { router }
